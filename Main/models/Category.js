4a2b1c17a18dd9ccb5deea26cee2a4a78cf0a5f0
Adrian Deon <sushdawg@Adrians-MBP.frontiernet.net>
2023-01-21 02:24:49 -0600

    finished cat model

--- Main/models/Category.js
+++ Main/models/Category.js
@@ -1,4 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
+const { Sequelize } = require('../config/connection.js');
 
 const sequelize = require('../config/connection.js');
 
@@ -6,7 +7,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull:false,
+      primaryKey:true,
+      autoIncrement:true,
+    },
+     category_name: {
+      type: DataTypes.STRING,
+      allowNull:false,
+    },
   },
   {
     sequelize,
